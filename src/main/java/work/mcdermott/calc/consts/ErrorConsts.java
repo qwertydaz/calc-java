332d77d3cfdcc104b2e60c418c4f19547ba78a72
Dáire McDermott <dairemcdermott@outlook.com>
2024-06-11 21:51:18 +0100

    extracts string value to constant

---
+++ src/main/java/work/mcdermott/calc/consts/ErrorConsts.java
@@ -0,0 +1,7 @@
+package work.mcdermott.calc.consts;
+
+public class ErrorConsts {
+    private ErrorConsts() {}
+
+    public static final String DIVIDE_BY_ZERO = "Cannot divide by zero";
+}
--- src/main/java/work/mcdermott/calc/request/Division.java
+++ src/main/java/work/mcdermott/calc/request/Division.java
@@ -2,13 +2,15 @@ package work.mcdermott.calc.request;
 
 import work.mcdermott.calc.err.OperationException;
 
+import static work.mcdermott.calc.consts.ErrorConsts.DIVIDE_BY_ZERO;
+
 public record Division(
         double dividend,
         double divisor
 
 ) {
     public Division {
-        if (divisor == 0.0) throw new OperationException("Cannot divide by zero");
+        if (divisor == 0.0) throw new OperationException(DIVIDE_BY_ZERO);
     }
 
     public double quotient() {
